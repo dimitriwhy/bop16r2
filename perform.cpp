51935fb010e2746af64fdbcdf7e2c22c59efec03
mayukuner <gmayukun@gmail.com>
2016-05-14 22:10:27 +0800

    modified perform.cpp

--- perform.cpp
+++ perform.cpp
@@ -62,18 +62,23 @@ vector<Path> get_ans(LL id1, LL id2){
     Paper p1, p2;
     string q1 = string("OR(Composite(AA.AuId=") + to_string(id1) + string("),AND(Y>0,Id=") + to_string(id1) + string("))");
     string q2 = string("OR(Composite(AA.AuId=") + to_string(id2) + string("),AND(Y>0,Id=") + to_string(id2) + string("))");
-    vector<Paper> chk = getEntities(string("OR(") + q1 + string(",") + q2 + string(")"), _ID|_F_FID|_J_JID|_C_CID|_AA_AUID|_RID |_CC | _AA_AFID | _RID);
+    vector<Paper> chk = getEntities(string("OR(") + q1 + string(",") + q2 + string(")"), _ID|_F_FID|_J_JID|_C_CID|_AA_AUID|_RID |_CC | _AA_AFID);
     vector<Paper> paper_by_au;
     for(auto &p : chk){
-        if(p.Id == id1 && p.AA.size()){
+        if(p.Id == id1){
             p1 = p;
             pp1 = true;
         }
-        if(p.Id == id2 && p.AA.size()){
+        if(p.Id == id2){
             p2 = p;
             pp2 = true;
         }
-        if(p.Id != id1 && p.Id != id2)
+        int flag = 0;
+        for(auto &author : p.AA)
+            if(author.AuId == id1 || author.AuId == id2){
+                flag = 1;
+            }
+        if(flag)
             paper_by_au.push_back(p);
     }
     if(pp1 && pp2)
